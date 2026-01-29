73408ed458617c3732d52f97513720390677187b
Hach Ngoc Hai <kiotarot@gmail.com>
2026-01-29 16:25:12 +0700

    calendar note version 1

--- js/dist/page.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function createPage(day) {
-  var page = document.createElement("div");
-  page.className = "calendar-page";
-  page.innerHTML = "\n    <div class=\"solar\">".concat(day.solar, "</div>\n    <div class=\"lunar\">").concat(day.lunar, "</div>\n    <div class=\"text\">").concat(day.text, "</div>\n  ");
-  return page;
-}
-//# sourceMappingURL=page.dev.js.map
